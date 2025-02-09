76e3f79b2769ca0c85488412b8e2c848529dc3e4
Akkarit Sangpetch <akkarit@cmkl.ac.th>
2025-02-09 13:37:38 +0700

    Update dependencies; pure-UEFI implementation

--- README.md
+++ README.md
@@ -2,13 +2,9 @@
 
 Based on examples provided in https://github.com/rust-osdev/bootloader
 
-To compile, ensure that your linux/wsl2 environment target is ready:
+To compile, ensure that your linux/wsl2 environment target is ready and you have qemu installed:
 ```
 sudo apt install qemu-system
-sudo apt install ovmf
-rustup target add x86_64-unknown-none
-rustup component add llvm-tools-preview
-rustup override set nightly
 ```
 
 The main project is configured with build-dependencies to build bootimage & run qemu when invoking `cargo build` or `cargo run`.
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,14 +1,16 @@
-#![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![no_std] // don't link the Rust standard library
 
+use core::arch::asm;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::fmt::Write;
+use core::panic::PanicInfo;
 use bootloader_api::info::{FrameBuffer, PixelFormat};
 use kernel::serial;
 use noto_sans_mono_bitmap::{FontWeight, get_raster, RasterHeight, RasterizedChar};
 
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     let _ = writeln!(serial(), "PANIC: {info}");
     loop {}
 }
@@ -17,13 +19,25 @@ const BOOTLOADER_CONFIG: BootloaderConfig = {
     config.kernel_stack_size = 100 * 1024; // 100 KiB kernel stack size
     config
 };
-entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
+entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    let rip = x86_64::registers::read_rip().as_u64();
+    writeln!(serial(), "RIP: 0x{:x}", rip).unwrap();
+    let rsp: u64;
+    unsafe {
+        asm!("mov {}, rsp", out(reg) rsp);
+    }
+    writeln!(serial(), "RSP: 0x{:x}", rsp).unwrap();
+    
     writeln!(serial(), "Entered kernel with boot info: {boot_info:?}").unwrap();
 
     let vga_buffer = boot_info.framebuffer.as_mut().unwrap();
-    // vga_buffer.buffer_mut().fill(0);
+    for x in 0..vga_buffer.info().width {
+        for y in 0..vga_buffer.info().height {
+            write_pixel(vga_buffer, x, y, RGBA((x % 256) as u8, (y%256) as u8, ((x + y) % 256) as u8, 0));
+        }
+    }
 
     let bitmap_char = get_raster('O', FontWeight::Regular, RasterHeight::Size16).unwrap();
     let next = write_rendered_char(vga_buffer, 0, 512, bitmap_char);
@@ -31,24 +45,31 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 
     writeln!(serial(), "Entering kernel wait loop...").unwrap();
 
-    loop {}
+    loop {
+        // This infinite loop keeps the kernel running
+    }
 }
 
-fn write_rendered_char(buffer:&mut FrameBuffer, x_pos:usize, y_pos:usize, rendered_char: RasterizedChar) -> (usize, usize) {
+struct Cursor(usize, usize);
+struct RGBA(u8, u8, u8, u8);
+
+fn write_rendered_char(buffer:&mut FrameBuffer, x_pos:usize, y_pos:usize, rendered_char: RasterizedChar) -> Cursor {
     for (y, row) in rendered_char.raster().iter().enumerate() {
         for (x, byte) in row.iter().enumerate() {
-            write_pixel(buffer, x_pos + x, y_pos + y, *byte);
+            write_pixel(buffer, x_pos + x, y_pos + y, intensity_to_rgba(*byte));
         }
     }
-    (x_pos + rendered_char.width(), y_pos)
+    Cursor(x_pos + rendered_char.width(), y_pos)
 }
 
-fn write_pixel(buffer:&mut FrameBuffer, x: usize, y: usize, intensity: u8) {
+fn intensity_to_rgba(intensity: u8) -> RGBA { RGBA(intensity >> 1, intensity, intensity >> 1, 0) }
+
+fn write_pixel(buffer:&mut FrameBuffer, x: usize, y: usize, rgba_color: RGBA) {
     let mut info = buffer.info();
     let pixel_offset = y * usize::from(info.stride) + x;
     let color = match info.pixel_format {
-        PixelFormat::Rgb => [intensity, intensity, intensity / 2, 0],
-        PixelFormat::Bgr => [intensity / 2, intensity, intensity, 0],
+        PixelFormat::Rgb => [rgba_color.0, rgba_color.1, rgba_color.2, rgba_color.3],
+        PixelFormat::Bgr => [rgba_color.0, rgba_color.1, rgba_color.2, rgba_color.3],
         other => {
             info.pixel_format = PixelFormat::Rgb;
             panic!("pixel format {:?} not supported in logger", other)
